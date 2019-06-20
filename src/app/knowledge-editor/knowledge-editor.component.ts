3f0d69a7bb02292e642b2912276a206b8286f83e
terrywang <helnawang@hotmail.com>
2019-06-20 14:06:03 +0800

    knowledge-editor页支持粘贴到文本框导入

--- src/app/knowledge-editor/knowledge-editor.component.ts
+++ src/app/knowledge-editor/knowledge-editor.component.ts
@@ -10,19 +10,27 @@ import {QuestionService} from '../question.service';
 })
 export class KnowledgeEditorComponent implements OnInit {
   questions: any[];
-  fileList: [] = [];
+  formGroup: FormGroup;
   constructor(private formBuilder: FormBuilder, qs: QuestionService) {
     this.questions = QuestionService.getQuestionsFromSetting();
   }
   ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      input_textarea: ['']
+    });
   }
-  importJSON(files) {
-    // 从系统中读取JSON文件
+  // 从系统中读取JSON文件
+  importJSONFromFile(files) {
     const file = files[0];
     const fileReader = new FileReader();
     fileReader.onload = (e) => {
-      this.questions = QuestionService.getQuestionValuesFromJson(JSON.parse(e.target['result'])['knowledgeList'][0], this.questions);
+      this.questions = QuestionService.getQuestionValuesFromJson(JSON.parse(e.target['result'])['knowledge'], this.questions);
     };
     fileReader.readAsText(file);
+  }
+  // 从输入框中读取JSON
+  importJSONFromTextarea() {
+    const value = this.formGroup.value.input_textarea;
+    this.questions = QuestionService.getQuestionValuesFromJson(JSON.parse(value)['knowledge'], this.questions);
   }
 }
