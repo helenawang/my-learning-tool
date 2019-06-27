c3dbdb121476bd151647fa62884ff9c2f44d4aa6
terrywang <helnawang@hotmail.com>
2019-06-27 17:39:25 +0800

    用async/await替代promise重构请求部分

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -1,20 +1,14 @@
 import {Injectable} from '@angular/core';
-import {QuestionBase} from './model/question-base';
 import {QuestionTextbox} from './model/question-textbox';
 import {QuestionTextarea} from './model/question-textarea';
-// import {QuestionSettings} from './model/Knowledge';
 import {QuestionTags} from './model/question-tags';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class QuestionService {
   constructor(private http: HttpClient) {}
-  // TODO make asynchronous
   // 工厂模式
-  dynamicFormFactory(type, name, required, description) {
+  static dynamicFormFactory(type, name, required, description) {
     switch (type) {
       case 'textbox':
         return new QuestionTextbox({ // TODO 这里有一些因名称不对应而手动实现的映射，后期是不是可以减少？让数据结构一致
@@ -37,14 +31,16 @@ export class QuestionService {
         });
     }
   }
-  private getQuestionSettings() {
-    return this.http.get('http://localhost:8080/questions/all').toPromise();
+
+  private constructQuestions(data) {
+    return  data
+      .map(q => QuestionService.dynamicFormFactory(q['type'], q['name'], q['required'], q['description']))
+      .sort((a, b) => a.order - b.order);
   }
-  getQuestionsFromSetting() {
-    return this.getQuestionSettings().then((data: any[]) => {
-        const questions = data.map(q => this.dynamicFormFactory(q['type'], q['name'], q['required'], q['description']));
-        return questions.sort((a, b) => a.order - b.order);
-    });
+  // I appreciate this function, elegant and clear
+  async getQuestionsFromSetting() {
+    const result = await this.http.get('http://localhost:8080/questions/all').toPromise();
+    return this.constructQuestions(result);
   }
   getQuestionValuesFromJson(json, questions: any[]) {
     const result = JSON.parse(JSON.stringify(questions)); // deep clone
